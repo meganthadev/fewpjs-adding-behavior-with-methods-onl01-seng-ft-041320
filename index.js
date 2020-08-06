4ef84e33739ce684317cb3b082f3a8894a7ca927
Megan Horsfield <meganthadev@gmail.com>
2020-08-06 08:27:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 class Cat {
   constructor(name, sex){
-    
+    speak(name){
+      return "${name} says meow!"
+    }
   }
 }
  
 class Dog {
   constructor(name, sex){
-    
+    speak(name){
+      return "${name} says woof!"
   }
 }
  
 class Bird {
   constructor(name, sex){
-    
+    speak(name, sex){
+      if (bird.sex === 'male' 
+        
+      
   }
 }
